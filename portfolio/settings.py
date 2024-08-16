888a1e209772ec36ed80a85e3c2fab31ae1b15d0
Vinayakdev06 <vinayakmishra9451@gmail.com>
2024-08-16 14:18:05 +0530

    Update configuration

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-b_a(e78%ux+#z0@9wv=!0bqy(qtrjpj9y0id#$r3-dh2@jj7)z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.ap']
+ALLOWED_HOSTS = ['portfolio-beta-ivory-99.vercel.app']
+
 
 
 # Application definition
@@ -117,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
