0ec199c5838ce0973071517a9300821d023bb48d
Vinayakdev06 <vinayakmishra9451@gmail.com>
2024-08-16 12:12:07 +0530

    changes on depolyment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b_a(e78%ux+#z0@9wv=!0bqy(qtrjpj9y0id#$r3-dh2@jj7)z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
